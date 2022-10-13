0d6dae868f5271de879ef830e83b6ed673b2152d
Malbamario <malba324@Gmail.com>
2022-10-13 20:07:03 +0800

    new tugas

---
+++ src/com/malba/algodat/kelas/tugas1/DriveThruCustomer.java
@@ -0,0 +1,5 @@
+package com.malba.algodat.kelas.tugas1;
+
+public class DriveThruCustommer {
+    Vehicle vehicle;
+}
---
+++ src/com/malba/algodat/kelas/tugas1/McDonalds.java
@@ -0,0 +1,2 @@
+package com.malba.algodat.kelas.tugas1;public class McDonalds {
+}
